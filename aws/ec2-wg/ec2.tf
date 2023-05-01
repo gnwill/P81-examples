9dc2a528091afe358bdf9a0b4efaf068572a0683
William Gonzalez <william@Williams-MacBook-Pro.local>
2023-05-01 18:23:14 -0400

    Fixed a comment for ami on ec2.tf

--- aws/ec2-wg/ec2.tf
+++ aws/ec2-wg/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "terraform-ec2" {
-  ami                         = "ami-0557a15b87f6559cf"
+  ami                         = "ami-0557a15b87f6559cf" # needs to be correct AMI for your region
   instance_type               = "t2.micro"
   key_name                    = "ssh-key"
   subnet_id                   = aws_subnet.terraform-subnet.id
