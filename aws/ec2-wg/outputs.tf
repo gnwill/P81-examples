d4a0f84611023d5b41464b4c6c94ef8361ae759b
Will <will@perimeter81.com>
2024-02-17 10:09:22 -0500

    cleaning up deleted files

--- aws/ec2-wg/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "public_ips" {
-  value = aws_instance.terraform-ec2.*.public_ip
-}
-
--- aws/single_tunnel_vgw/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "instance_ip" {
-  description = "The public ip for ssh access"
-  value       = aws_instance.terraform-ec2.public_ip
-}
